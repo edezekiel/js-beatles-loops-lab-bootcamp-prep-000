89b0ed1a94c1aae402671302ec670c53c72dc9bd
ed.a.ezekiel@gmail.com <ed.a.ezekiel@gmail.com>
2018-09-08 19:02:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,16 +3,11 @@
 //for loop
 
 function theBeatlesPlay(musicians, instruments){
-
-var newArray = [];
-
-for (let i = 0; i < musicians.length ; i++){
-
-newArray.push(musicians[i] + " plays " + instruments[i])
-  }
-
-return newArray
-
+  var newArray = [];
+  for (let i = 0; i < musicians.length ; i++){
+    newArray.push(musicians[i] + " plays " + instruments[i])
+    }
+    return newArray
 }
 
 
